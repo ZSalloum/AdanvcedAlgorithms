dc629cda8686b6fdbc6e87e730ebed7689ec5912
zsalloum <zsalloum@hotmail.com>
2016-03-22 00:57:26 +0100

    Fix GraphUnitTest

--- Algorithms.Toolkit.UnitTest/GraphUnitTest.cs
+++ Algorithms.Toolkit.UnitTest/GraphUnitTest.cs
@@ -2,7 +2,6 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NSubstitute;
 using Algorithms.Toolkit.Graphs;
 
 namespace Algorithms.Toolkit.UnitTest
